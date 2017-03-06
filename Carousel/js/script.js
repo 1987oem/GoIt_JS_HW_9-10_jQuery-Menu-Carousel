fbfe28eb12056fee104f0aab4b81870b93c7ee33
Ujin <1987oem@gmail.com>
2017-03-06 15:51:25 +0200

    Update script.js

--- Carousel/js/script.js
+++ Carousel/js/script.js
@@ -1,5 +1,26 @@
 $(function () {
 	$('.jcarousel').jcarousel({
 		items: '.jcarousel-item'
-	})
-})
+	});
+	$('.jcarousel-prev').jcarouselControl({target:'-=1'});
+	$('.jcarousel-next').jcarouselControl({target:'+=1'});
+	$('.left_arrow').jcarouselControl({target:'-=1'});
+	$('.right_arrow').jcarouselControl({target:'+=1'});
+});
+
+$(function () {
+	$('.jcarousel').jcarousel({
+		list: '.jcarousel-list'
+	});
+	$('.jcarousel-prev').jcarouselControl({traget:'-=1'});
+	$('.jcarousel-next').jcarouselControl({traget:'-=1'});
+	$('.left_arrow').jcarouselControl({target:'-=1'});
+	$('.right_arrow').jcarouselControl({target:'+=1'});
+});
+
+$('select').selctbox();
+$(function () {
+	$('.container').click(function () {
+		$('.div_for_checkbox').toggleClass('active');
+	});
+});
